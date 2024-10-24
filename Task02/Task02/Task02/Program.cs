bc5810cfe8b7972811eea04050e94284e946ac6f
Lika Akhalaia <liaakhalaia@credo.ge>
2024-10-24 15:48:48 +0400

    HW2 -Data Types, "About me"

---
+++ Task02/Task02/Task02/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("My name is Lika, and here is some information about myself:");
+
+        byte b = 31; // Age
+        Console.WriteLine("Age: " + b);
+
+        string N = "01010101011";
+        Console.WriteLine("ID: " + N);
+
+        string Country = "Georgia";
+        string City = "Tbilisi";
+        string Address = "Iosebidze N123";
+        short Zip = 1171;
+        string comma = ", ";
+        Console.WriteLine("Address: " + Country + comma + City + comma + Address + comma + Zip);
+
+        float H = 1.7F; //Hight;
+        Console.WriteLine("height: " + H + " meters");
+
+        bool IsMarried = true;
+        Console.WriteLine("Marital Status:" + IsMarried);
+
+        String DC = "Debit Card";
+        bool D = true;
+        Console.WriteLine(DC + ":" + D);
+
+        string NameC = "Account Number: ";
+        string Iban = "CD";
+        string code = "GE40";
+        string codeF = "033000001111111";
+        Console.WriteLine(NameC + code + Iban + codeF);
+
+
+    }
+}
