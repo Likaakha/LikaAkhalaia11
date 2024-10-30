1c69323f7ec8717ef5f9e5a7f9836b05dffe3659
Lika Akhalaia <liaakhalaia@credo.ge>
2024-10-30 04:15:40 +0400

    Task05 - Arays

---
+++ Task0000/Task05/Task05/Program.cs
@@ -0,0 +1,53 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Task 1:
+
+        string[] students = new string[3];
+
+        for (int i = 0; i < students.Length; i++)
+        {
+            string input = Console.ReadLine();
+            students[i] = input;
+
+        }
+        for (int i = 0; i < students.Length; i++)
+        {
+            Console.Write(students[i] + ", ");
+        }
+
+
+        //Task N2
+
+        int[,] grades = new int[3, 2];
+        string[] studentNames = { " Lika", " Nika", " Vika " };
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("Enter scores for" + studentNames[i] + ":");
+
+            for (int j = 0; j < 2; j++)
+            {
+                Console.Write("Score for Subject" + (j+1) + ": " );
+                grades[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("Student Grades:");
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write(studentNames[i] + "- ");
+            for (int j = 0; j < 2; j++)
+            {
+                Console.Write(grades[i, j] + "  ");
+            }
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
