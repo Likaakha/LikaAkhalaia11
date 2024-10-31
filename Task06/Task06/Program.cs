36dc62e51c06dc022843128c1cc95d072a56f5d0
Lika Akhalaia <liaakhalaia@credo.ge>
2024-10-31 20:39:03 +0400

    Added Task06, new method for array

---
+++ Task06/Task06/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Task06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //5 elementiani masivis shekmna 
+            int[] arr = new int[5];
+
+            // ricxvebis sheyvana
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Enter Numbers: ");
+                int num = int.Parse(Console.ReadLine());
+                arr[i] = num;
+            }
+
+            // indeqsis sheyvana
+            Console.WriteLine("Now enter index: ");
+            int index = int.Parse(Console.ReadLine());
+
+            // shedegis dabechdva
+            Console.WriteLine("Shedegi: " + CalculateNumberSum(arr, index));
+
+
+        }
+
+        static int CalculateNumberSum(int[] array, int idx) //axali metodi
+        {
+            int n = 0;
+
+            {
+                n = array[idx]; // rac unda davamushavot
+            }
+
+            int result = 0;
+
+            while (true)
+            {
+                if (n == 0)
+                {
+                    break;
+                }
+
+                result += n % 10;
+                n /= 10;
+            }
+
+            return result;
+        }
+
+    }
+}
+
