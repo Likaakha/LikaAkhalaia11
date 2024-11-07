396f5ed14910786ee6c55f11a9b1f2b40ff56971
Lika Akhalaia <liaakhalaia@credo.ge>
2024-11-08 03:53:41 +0400

    Task08- Stack, Heap

---
+++ Task08/Task08/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task08
+{
+  
+        public class Person
+        {
+
+            public Person(int age, string name)
+            {
+                Age = age;
+                Name = name;
+            }
+
+            public int Age { get; set; }
+            public string Name { get; set; }
+
+        }
+
+        internal class Program1
+        {
+
+            static void Main()
+            {
+                Person person1 = new Person(31, "Lika");
+
+                ChangePerson(person1);
+                Console.WriteLine(person1.Age + " " + person1.Name);
+            }
+
+            static void ChangePerson(Person p)
+            {
+                p.Age++;
+                p.Name = "Nika";
+            }
+        }
+    }
+
+
+
